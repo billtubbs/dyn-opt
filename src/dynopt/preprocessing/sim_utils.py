219278c6e0ee5edaa1749ec8e52465b607250a20
Bill Tubbs <bill.tubbs@me.com>
2023-12-16 15:25:24 -0800

    Debugged DataLogger

--- src/dynopt/preprocessing/sim_utils.py
+++ src/dynopt/preprocessing/sim_utils.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
+from pandas.api.types import is_integer_dtype
 
 
-class RealTimeDataLogger():
+class DataLogger():
     k: int
     data: pd.DataFrame
     time_var: str
@@ -13,48 +14,42 @@ class RealTimeDataLogger():
     k_name: str
     nT_init: int
 
-    def __init__(self, initial_values, time_var='t', nT_max=100, 
-                 nT_ahead=0, k_start=0, k_name='k'):
-        self.initial_values = initial_values
-        self.time_var = time_var
+    def __init__(self, data=None, sample_time=None, columns=None, dtype=None, 
+                 t_name='t', ts_name='k', k_first=None, nT_max=100, nT_ahead=0, 
+                 **kwargs):
+        df = pd.DataFrame(data, columns=columns, dtype=dtype, **kwargs)
+        assert is_integer_dtype(df.index), "index values must be integers"
+        assert df.index.duplicated().sum() == 0, "index contains duplicates"
+        df = df.sort_index()
+        df.index.name = ts_name
+        nT_init = df.index.shape[0]
+        if k_first is None:
+            k_first = df.index[0]
+        if t_name not in df:
+            if df.shape[0] > 1:
+                if sample_time is None:
+                    raise ValueError("provide time values in data or sample_time")
+                k = df.index.values + (k_first - df.index[0])
+                df.insert(0, t_name, k * sample_time)
+            else:
+                df.insert(0, t_name, 0)
+        assert nT_ahead < nT_max, "nT_max too small for look-ahead horizon"
+        if df.shape[0] < nT_max:
+            new_index = pd.RangeIndex(df.index[0], df.index[0] + nT_max, name=ts_name)
+            new_df = pd.DataFrame(np.nan, index=new_index, columns=df.columns, dtype=dtype)
+            new_df.loc[df.index] = df.values
+            df = new_df
+        if k_first != df.index[0]:
+            df.index += (k_first - df.index[0])
+        self.data = df
+        self.sample_time = sample_time
+        self.t_name = t_name
+        self.ts_name = ts_name
+        self.k_first = k_first
         self.nT_max = nT_max
         self.nT_ahead = nT_ahead
-        self.k_start = k_start
-        self.k_name = k_name
-        if time_var not in initial_values:
-            initial_values[time_var] = 0
-        self.reset()
-
-    def reset(self):
-        self.k = self.k_start
-        nT_init = 1
-        # Find length of longest sequence (if any) in initial_values
-        for name in self.initial_values:
-            try:
-                nT_init = max(nT_init, len(self.initial_values[name]))
-            except TypeError:
-                pass
         self.nT_init = nT_init
-        index = np.arange(self.nT_max)
-        index += self.k_start - self.nT_init + 1
-        self.data = pd.DataFrame(
-            np.nan, 
-            columns=self.initial_values.keys(),
-            index=pd.Index(index, name=self.k_name)
-        )
-        self.data.insert(self.k_start, self.time_var, self.data.pop(self.time_var))
-        if nT_init == 1:
-            self.data.loc[self.k] = self.initial_values
-        else:
-            # Assume sequence values in initial_values are for previous 
-            # time instances up to and including the start time
-            for name in self.initial_values:
-                try:
-                    nT = len(self.initial_values[name])
-                except TypeError:
-                    self.data.loc[self.k, name] = self.initial_values[name]
-                else:
-                    self.data.loc[self.k-nT+1:self.k, name] = self.initial_values[name]
+        self.k = k_first + nT_init - 1
 
     def append(self, *args):
         if len(args) == 2:
@@ -65,7 +60,7 @@ class RealTimeDataLogger():
             #TODO: Support dataframes
             else:
                 d = dict(args[0])
-                rows = [(d[self.time_var], {k: d[k] for k in d if k != self.time_var})]
+                rows = [(d[self.t_name], {k: d[k] for k in d if k != self.t_name})]
         else:
             raise TypeError("invalid arguments")
         for t, data in rows:
@@ -75,7 +70,10 @@ class RealTimeDataLogger():
                 self.data.index += 1
             for name in data:
                 self.data.loc[self.k, name] = data[name]
-            self.data.loc[self.k, self.time_var] = t
+            self.data.loc[self.k, self.t_name] = t
 
-    def __getitem__(self, key):
-         return self.data.loc[key]
+    # TODO: Is this needed?  Would take up memory storing initial values
+    # def reset(self):
+
+    #     self.data.iloc[self.nT_init:] = None
+    #     self.k = self.data.index[0] + self.nT_init - 1
--- tests/test_sim_utils.py
+++ tests/test_sim_utils.py
@@ -4,118 +4,239 @@ import pandas as pd
 from pandas.testing import assert_frame_equal, assert_series_equal
 from numpy.testing import assert_array_equal
 
-from dynopt.preprocessing.sim_utils import RealTimeDataLogger
+from dynopt.preprocessing.sim_utils import DataLogger
 
-class RealTimeDataLoggerTests(unittest.TestCase):
+class DataLoggerTests(unittest.TestCase):
 
     def test_initialization(self):
-        initial_values = {'x': 0.0, 'y': np.nan}
-        rtdl = RealTimeDataLogger(initial_values)
+        initial_values = {'x': [0.0], 'y': [np.nan]}
+        dl = DataLogger(initial_values)
 
-        assert rtdl.k_start == 0
-        assert rtdl.nT_init == 1
-        assert rtdl.nT_max == 100
-        assert rtdl.nT_ahead == 0
-        assert rtdl.data.shape == (rtdl.nT_max, 3)
-        assert_array_equal(rtdl.data.index.to_numpy(), np.arange(100))
-        assert rtdl.data.index.name == 'k'
+        assert dl.k_first == 0
+        assert dl.nT_init == 1
+        assert dl.k == 0
+        assert dl.nT_max == 100
+        assert dl.nT_ahead == 0
+        assert dl.data.shape == (dl.nT_max, 3)
+        assert_array_equal(dl.data.index.to_numpy(), np.arange(100))
+        assert dl.data.index.name == 'k'
         assert_series_equal(
-            rtdl.data.loc[0][initial_values.keys()], 
-            pd.Series(initial_values), 
+            dl.data.loc[0][initial_values.keys()], 
+            pd.DataFrame(initial_values).loc[0], 
             check_names=False
         )
 
-        initial_values = {'t': 10.0, 'x': 0.0, 'y': np.nan}
-        rtdl = RealTimeDataLogger(initial_values, nT_max=5)
+        initial_values = {'t': 15.0, 'x': 0.0, 'y': np.nan}
+        dl = DataLogger(initial_values, index=[0], nT_max=5)
+        assert dl.k_first == 0
+        assert dl.nT_init == 1
+        assert dl.k == 0
         assert_frame_equal(
-            rtdl.data,
+            dl.data,
             pd.DataFrame(
                 np.array([
-                    [   10.,     0., np.nan],
+                    [   15.,     0., np.nan],
                     [np.nan, np.nan, np.nan],
                     [np.nan, np.nan, np.nan],
                     [np.nan, np.nan, np.nan],
                     [np.nan, np.nan, np.nan]
                 ]),
-                index=pd.Index(range(5), name='k'),
+                index=pd.RangeIndex(0, 5, name='k'),
                 columns=['t', 'x', 'y']
             )
         )
+
+        initial_values = {
+            't': [15.0, 16.5, 18.001], 
+            'x': [0.0, 0.1, 0.2],
+            'y': [None, 1.5, None]
+        }
+        dl = DataLogger(initial_values, nT_max=5)
+        assert dl.k_first == 0
+        assert dl.nT_init == 3
+        assert dl.k == 2
+        assert_frame_equal(
+            dl.data,
+            pd.DataFrame(
+                np.array([
+                    [   15.0,   0.0, np.nan],
+                    [   16.5,   0.1,    1.5],
+                    [ 18.001,   0.2, np.nan],
+                    [np.nan, np.nan, np.nan],
+                    [np.nan, np.nan, np.nan]
+                ]),
+                index=pd.RangeIndex(0, 5, name='k'),
+                columns=['t', 'x', 'y']
+            )
+        )
+
+        initial_values = {
+            'x': [0.0, 0.1, 0.2],
+            'y': [None, 1.5, None]
+        }
+        dl = DataLogger(initial_values, sample_time=1.5, nT_max=5)
+        assert dl.k_first == 0
+        assert dl.nT_init == 3
+        assert dl.k == 2
+        assert_frame_equal(
+            dl.data,
+            pd.DataFrame(
+                np.array([
+                    [   0.0,    0.0,  np.nan],
+                    [   1.5,    0.1,    1.5],
+                    [   3.0,    0.2, np.nan],
+                    [np.nan, np.nan, np.nan],
+                    [np.nan, np.nan, np.nan]
+                ]),
+                index=pd.RangeIndex(0, 5, name='k'),
+                columns=['t', 'x', 'y']
+            )
+        )
+
+        dl = DataLogger(initial_values, sample_time=1.5, nT_max=5,
+                        k_first=10)
+        assert dl.k_first == 10
+        assert dl.nT_init == 3
+        assert dl.k == 12
+        assert_frame_equal(
+            dl.data,
+            pd.DataFrame(
+                np.array([
+                    [   15.0,   0.0, np.nan],
+                    [   16.5,   0.1,    1.5],
+                    [   18.0,   0.2, np.nan],
+                    [np.nan, np.nan, np.nan],
+                    [np.nan, np.nan, np.nan]
+                ]),
+                index=pd.RangeIndex(10, 15, name='k'),
+                columns=['t', 'x', 'y']
+            )
+        )
+
+        initial_values = {
+            'x': [0.0, 0.1, 0.2],
+            'y': [None, 1.5, None]
+        }
+        index = [-2, -1, 0]  # include values before k=0
+        dl = DataLogger(initial_values, index=index, sample_time=1.5,
+                        nT_max=5)
+        assert dl.k_first == -2
+        assert dl.nT_init == 3
+        assert dl.k == 0
+        assert_frame_equal(
+            dl.data,
+            pd.DataFrame(
+                np.array([
+                    [   -3.0,   0.0, np.nan],
+                    [   -1.5,   0.1,    1.5],
+                    [    0.0,   0.2, np.nan],
+                    [np.nan, np.nan, np.nan],
+                    [np.nan, np.nan, np.nan]
+                ]),
+                index=pd.RangeIndex(-2, 3, name='k'),
+                columns=['t', 'x', 'y']
+            )
+        )
+
+        dl = DataLogger(initial_values, sample_time=1.5, k_first=-2,
+                        nT_max=5)
+        assert dl.k_first == -2
+        assert dl.nT_init == 3
+        assert dl.k == 0
+        assert_frame_equal(
+            dl.data,
+            pd.DataFrame(
+                np.array([
+                    [   -3.0,   0.0, np.nan],
+                    [   -1.5,   0.1,    1.5],
+                    [    0.0,   0.2, np.nan],
+                    [np.nan, np.nan, np.nan],
+                    [np.nan, np.nan, np.nan]
+                ]),
+                index=pd.RangeIndex(-2, 3, name='k'),
+                columns=['t', 'x', 'y']
+            )
+        )
+
 
     def test_append(self):
 
-        initial_values = {'t': 10.0, 'x': 0.0, 'y': np.nan}
-        rtdl = RealTimeDataLogger(initial_values, nT_max=5)
+        initial_values = {'t': [15.0], 'x': [0.0], 'y': [np.nan]}
+        dl = DataLogger(initial_values, nT_max=5)
 
-        rtdl.append(11, {'x': 0.1, 'y': 1.5})
-        assert rtdl.k == 1
-        rtdl.append({'t': 12.01, 'x': 0.2, 'y': np.nan})  # Alternative
-        assert rtdl.k == 2
+        dl.append(16.5, {'x': 0.1, 'y': 1.5})
+        assert dl.k == 1
+        dl.append({'t': 18.001, 'x': 0.2, 'y': np.nan})  # Alternative
+        assert dl.k == 2
         assert_frame_equal(
-            rtdl.data,
+            dl.data,
             pd.DataFrame(
                 np.array([
-                    [   10.,     0., np.nan],
-                    [   11.,    0.1,    1.5],
-                    [ 12.01,    0.2, np.nan],
-                    [np.nan, np.nan, np.nan],
-                    [np.nan, np.nan, np.nan]
+                    [   15.0,    0.0,  np.nan],
+                    [   16.5,    0.1,     1.5],
+                    [ 18.001,    0.2,  np.nan],
+                    [ np.nan,  np.nan,  np.nan],
+                    [ np.nan,  np.nan,  np.nan]
                 ]),
-                index=pd.Index(range(5), name='k'),
+                index=pd.RangeIndex(0, 5, name='k'),
                 columns=['t', 'x', 'y']
             )
         )
 
-        rtdl.append([
-            (13.01, {'x': 0.3, 'y': 1.7}),
-            (13.98, {'x': 0.4, 'y': 1.8})
+        dl.append([
+            (19.501, {'x': 0.3, 'y': 1.7}),
+            (20.987, {'x': 0.4, 'y': 1.8})
         ])
-        assert rtdl.k == 4
+        assert dl.k == 4
         assert_frame_equal(
-            rtdl.data,
+            dl.data,
             pd.DataFrame(
                 np.array([
-                    [   10.,     0., np.nan],
-                    [   11.,    0.1,    1.5],
-                    [ 12.01,    0.2, np.nan],
-                    [ 13.01,    0.3,    1.7],
-                    [ 13.98,    0.4,    1.8]
+                    [   15.0,    0.0,  np.nan],
+                    [   16.5,    0.1,     1.5],
+                    [ 18.001,    0.2,  np.nan],
+                    [ 19.501,    0.3,     1.7],
+                    [ 20.987,    0.4,     1.8]
                 ]),
-                index=pd.Index(range(5), name='k'),
+                index=pd.RangeIndex(0, 5, name='k'),
                 columns=['t', 'x', 'y']
             )
         )
 
-        rtdl.append(pd.Series({'t': 14.97, 'x': 0.5, 'y': 1.9}))
-        assert rtdl.k == 5
+        dl.append(pd.Series({'t': 22.488, 'x': 0.5, 'y': 1.9}))
+        assert dl.k == 5
         assert_frame_equal(
-            rtdl.data,
+            dl.data,
             pd.DataFrame(
                 np.array([
-                    [   11.,    0.1,    1.5],
-                    [ 12.01,    0.2, np.nan],
-                    [ 13.01,    0.3,    1.7],
-                    [ 13.98,    0.4,    1.8],
-                    [ 14.97,    0.5,    1.9]
+                    [   16.5,    0.1,     1.5],
+                    [ 18.001,    0.2,  np.nan],
+                    [ 19.501,    0.3,     1.7],
+                    [ 20.987,    0.4,     1.8],
+                    [ 22.488,    0.5,    1.9]
                 ]),
-                index=pd.Index(range(1, 6), name='k'),
+                index=pd.RangeIndex(1, 6, name='k'),
                 columns=['t', 'x', 'y']
             )
         )
 
-        rtdl.reset()
-        assert_frame_equal(
-            rtdl.data,
-            pd.DataFrame(
-                np.array([
-                    [   10.,     0., np.nan],
-                    [np.nan, np.nan, np.nan],
-                    [np.nan, np.nan, np.nan],
-                    [np.nan, np.nan, np.nan],
-                    [np.nan, np.nan, np.nan]
-                ]),
-                index=pd.Index(range(5), name='k'),
-                columns=['t', 'x', 'y']
-            )
-        )
-        assert rtdl.k == rtdl.k_start
+        # dl.reset()
+        # assert_frame_equal(
+        #     dl.data,
+        #     pd.DataFrame(
+        #         np.array([
+        #             [   10.,     0., np.nan],
+        #             [np.nan, np.nan, np.nan],
+        #             [np.nan, np.nan, np.nan],
+        #             [np.nan, np.nan, np.nan],
+        #             [np.nan, np.nan, np.nan]
+        #         ]),
+        #         index=pd.RangeIndex(0, 5, name='k'),
+        #         columns=['t', 'x', 'y']
+        #     )
+        # )
+        # assert dl.k == dl.k_first
+
+
+if __name__ == "__main__":
+    unittest.main()
